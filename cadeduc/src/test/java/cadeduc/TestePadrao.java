b03309369cbaf5e1167356073e7814ed9634a1ee
estherbarbara <estherbarbara@A204-14.imd.local>
2018-06-20 16:24:27 -0300

    #Teste de teste

---
+++ cadeduc/src/test/java/cadeduc/TestePadrao.java
@@ -0,0 +1,14 @@
+package cadeduc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestePadrao {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
